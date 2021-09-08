38050c61ed37cffad8aa5cd9c8e64ffe47203c31
Marc Kohaupt <marc.kohaupt@avm-d.de>
2021-09-08 15:14:03 +0200

    Ensure configured directories do exist; create them if not.

--- app/library/configuration.py
+++ app/library/configuration.py
@@ -1,4 +1,5 @@
 import logging.config
+import os
 from functools import lru_cache
 from typing import Optional
 
@@ -21,11 +22,21 @@ def get_configuration():
     return configuration
 
 
+def ensure_directory_exists(path: str):
+    logger.debug(f"Ensure configured directory {path} exists...")
+
+    if not os.path.isdir(path):
+        logger.warning(f"Configured directory '{path}' does not exist, creating directory...")
+        os.makedirs(path, exist_ok=True)
+
+
 class Configuration(BaseSettings):
     inkscape_url: str
+
     data_directory: str = "data/"
     doorplates_directory: Optional[str]
     templates_directory: Optional[str]
+
     rendering_backend: str = "inkscape-microservice"
     # rendering_backend: str = "inkscape"
     # rendering_backend: str = "cairosvg"
@@ -35,19 +46,23 @@ class Configuration(BaseSettings):
         if self.doorplates_directory is None:
             doorplates_directory = f"{self.data_directory}/doorplates/"
             logger.debug(f"Doorplates directory not specified. Using data directory: {doorplates_directory}")
-            return doorplates_directory
         else:
-            logger.debug(f"Doorplates directory specified: {self.doorplates_directory}")
-            return self.doorplates_directory
+            doorplates_directory = self.doorplates_directory
+            logger.debug(f"Doorplates directory specified: {doorplates_directory}")
+
+        ensure_directory_exists(doorplates_directory)
+        return doorplates_directory
 
     def get_templates_directory(self):
         if self.templates_directory is None:
             templates_directory = f"{self.data_directory}/templates/"
             logger.debug(f"Templates directory not specified. Using data directory: {templates_directory}")
-            return templates_directory
         else:
+            templates_directory = self.templates_directory
             logger.debug(f"Templates directory specified: {self.templates_directory}")
-            return self.templates_directory
+
+        ensure_directory_exists(templates_directory)
+        return templates_directory
 
     class Config:
         env_file = "configuration.env"
